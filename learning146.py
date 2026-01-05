225bab964f691937417004a1e81e6cec96da87d2
Taeyong <rnjsxodyd231@naver.com>
2026-01-05 09:14:41 +0900

    코딩테스트 연습

---
+++ learning146.py
@@ -0,0 +1,22 @@
+def solution(n, times):
+    left = 1
+    right = max(times) * n
+    answer = right
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        # mid 시간 동안 처리 가능한 사람 수
+        total = 0
+        for t in times:
+            total += mid // t
+            if total >= n:
+                break
+
+        if total >= n:
+            answer = mid
+            right = mid - 1  # 더 작은 시간 탐색
+        else:
+            left = mid + 1   # 시간이 부족하므로 늘림
+
+    return answer
